17116428744294614d030c4afb16529fcc329afe
wendy-wm-wu <wendy.wm.wu@gmail.com>
2020-02-16 10:57:16 -0800

    Set up Babel and Webpack to bundle JavaScript files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './src/javascript/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
+
