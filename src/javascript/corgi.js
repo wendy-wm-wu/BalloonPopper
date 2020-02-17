089c383e6bb15d53c750b0627ef75833e6aa5a8f
wendy-wm-wu <wendy.wm.wu@gmail.com>
2020-02-16 20:44:00 -0800

    Add bread and happy corgi images and create Corgi class to generate corgi and include corgi reactions

--- src/javascript/corgi.js
+++ src/javascript/corgi.js
@@ -1,3 +1,18 @@
+const corgiImages = {
+  0: '../images/corgi.png',
+  1: '../images/corgi1.png',
+  2: '../images/corgi2.png',
+  3: '../images/bread.png',
+  4: '../images/bread1.png',
+}
+
+const candyImages = {
+  0: '../images/candy.png',
+  1: '../images/candy2.png',
+  2: '../images/candy3.png',
+  3: '../images/candy4.png',
+}
+
 class Corgi {
   constructor(canvas, stage, score, stats) {
     this.canvas = canvas;
@@ -8,30 +23,74 @@ class Corgi {
     const dirs = [1, -1];
     this.direction = dirs[Math.floor(Math.random() * 2)]; 
   }
-  generateBalloon = interval => {
-    const randomKey = Math.floor(Math.random() * 6);  
-    //TODO: set balloon variable
 
-    if (randomKey === 4 || randomKey === 5) {
-      corgi.type = "corgi";
+  generateCorgi = interval => {
+    const randomKey = Math.floor(Math.random() * 5);  
+    const corgi = new createjs.Bitmap(corgiImages[randomKey])
+
+    if (randomKey === 3 || randomKey === 4) {
+      corgi.type = "bomb";
       this.stats.increaseTotalBombs();
     } else {
-      corgi.type = "bomb";
-      this.stats.increaseTotalBalloons(); 
+      corgi.type = "corgi";
+      this.stats.increaseTotalCorgis(); 
     }
 
+    const tag = new createjs.Shape(); 
+    tag.beginFill('#000').drawRect(0, 20, 90, 70); 
+    corgi.hitArea = tag; 
+
+    corgi.x = Math.round(Math.random() * this.canvas.width);
+    corgi.y = 800; 
+    corgi.rotation = Math.random() * 360; 
+    this.stage.addChild(this.corgi); 
+    
+    return corgi; 
   };
 
-  deleteBalloon = () => {
-
+  addListener = (corgi, interval) => {
+    corgi.addEventListener('mouseover', () => {
+      const type = corgi.type; 
+      
+      this.corgiReaction(corgi, type); 
+      this.deleteCorgi(corgi, interval);
+      this.stage.update();
+    });
   };
+  
+  corgiReaction = (corgi, type) => {
+    this.score.updateScore(type);
 
-  popBalloon = () => {
-
+    if (type === "corgi") {
+      this.tagCorgi(corgi);
+      this.dropCandy(corgi);
+      this.stats.increaseTagCorgis();
+    } else if (type === "bomb") {
+      this.stats.increaseHitBombs();
+      //warning sign to be careful
+      //setinterval to show warning sign 
+    }
   };
-
-  balloonReaction = () => {
-
+  
+  tagCorgi = corgi => {
+    const candy = new createjs.Bitmap('../images/happy_corgi.png');
+    candy.x = corgi.x;
+    candy.y = corgi.y; 
+    this.stage.addChild(candy);
+    setTimeout(() => this.stage.removeChild(candy), 750); 
+  };
+  
+  dropCandy = corgi => {
+    const randomKey = Math.floor(Math.random() * 4);
+    const droppedCandy = new createjs.Bitmap(candyImages[randomKey]); 
+    droppedCandy.x = corgi.x; 
+    droppedCandy.y = (Math.random() * 30) + 680; 
+    this.stage.addChild(droppedCandy); 
+  };
+  
+  deleteCorgi = (corgi, interval) => {
+    this.stage.removeChild(corgi); 
+    clearInterval(interval); 
   };
 }
 
